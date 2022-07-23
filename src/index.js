39a16400d5df3f04b60d406a85c3f0817612b25a
Milos Savic <msavic248@yahoo.com>
2022-07-23 18:52:03 +0200

    Final API fix

--- src/index.js
+++ src/index.js
@@ -5,24 +5,6 @@ import AppData from "./app-data.js";
 import "snackbar/dist/snackbar.min.css";
 import Chart from "chart.js/auto";
 
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
-const firebaseConfig = {
-  // ...
-  // The value of `databaseURL` depends on the location of the database
-  databaseURL: "https://track-calories-app-default-rtdb.europe-west1.firebasedatabase.app",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
-// Initialize Realtime Database and get a reference to the service
-const database = getDatabase(app);
-
 const API = new FetchWrapper(
   "https://firestore.googleapis.com/v1/projects/track-calories-app-357216"
 );
